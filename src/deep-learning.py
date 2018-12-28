c093846b24a1c1df6c765c0161442a4fa9670029
Nuno <nunojdcm@fe.up.pt>
2018-12-28 16:07:16 +0000

    basic funcs

--- src/deep-learning.py
+++ src/deep-learning.py
@@ -1,23 +1,15 @@
 from shared import *
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+IMG_PATH = '../images'
+ANNOT_PATH = '../annotations'
 
 def main():
-    print('ola')
+    global IMG_PATH
+    global ANNOT_PATH
+
+    db = Database(IMG_PATH, ANNOT_PATH)
+
+    img = db.read_img_region('arrabida-0000')
 
 
 if __name__ == "__main__":
--- src/shared.py
+++ src/shared.py
@@ -11,22 +11,41 @@ class Database():
 
     # Use name like 'arrabida-0001'
     def annot_coords(self, name):
-        annotPath = glob.glob(self.path_annot + '/*/' + name + '.xml')[0]
-        tree = ET.parse(annotPath)
-        xmin = int(tree.find('xmin').text)
-        xmax = int(tree.find('xmax').text)
-        ymin = int(tree.find('ymin').text)
-        ymax = int(tree.find('ymax').text)
-        return (xmin,xmax,ymin,ymax)
+        annotPath = glob.glob(self.path_annot + '/**/' + name + '.xml', recursive=True)[0]
+        try:
+            tree = ET.parse(annotPath)
+            root = tree.getroot()
+            branch = root.find('object').find('bndbox')
+            xmin = int(branch.find('xmin').text)
+            xmax = int(branch.find('xmax').text)
+            ymin = int(branch.find('ymin').text)
+            ymax = int(branch.find('ymax').text)
+            return (xmin,xmax,ymin,ymax)
+        except:
+            return (0,0,0,0)
     
     # Use name like 'arrabida-0001'
     def read_img(self, name):
-        imagePath = glob.glob(self.path_imgs + '/*/' + name + '.*')[0]
+        imagePath = glob.glob(self.path_imgs + '/**/' + name + '.*', recursive=True)[0]
         img = cv2.imread(imagePath)
         return img
+    
+    # Get image with annotated region
+    def read_img_region(self, name):
+        img = self.read_img(name)
+        xmin,xmax,ymin,ymax = self.annot_coords(name)
+        if xmin == xmax and xmax == ymin and ymin == ymax and ymax == 0:
+            return img
+        else:
+            try:
+                img2 = img_region(img,xmin,xmax,ymin,ymax)
+                return img2
+            except:
+                return img
+
 
 # Extract image region
- def img_region(img, xmin, xmax, ymin, ymax):
+def img_region(img, xmin, xmax, ymin, ymax):
     return img[ymin:ymax,xmin:xmax]
 
 if __name__ == "__main__":
